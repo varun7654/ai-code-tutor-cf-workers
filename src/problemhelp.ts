a5aff7a4a77269c7afb7d4e174ecb91a96913621
varun7654 <varun7654@outlook.com>
2024-03-31 16:47:03 -0400

    Give the ai some memory

--- src/problemhelp.ts
+++ src/problemhelp.ts
@@ -26,6 +26,7 @@ export class UserData {
     testResults: TestResults = new TestResults();
     lastUpdated: Date = new Date();
     currentCode: string = null as unknown as string;
+    aiRememberResponse: string[] = [];
 
     constructor(history: string[] = [], requestHelpHistory: string[] = [],  testResults: TestResults = new TestResults(), lastUpdated: Date = new Date(), currentCode: string = "") {
         this.history = history;
@@ -74,17 +75,38 @@ let systemMessage = "You are a helpful tutor on a website that is teaching peopl
     "If the code is working give the user a quick congratulations.\n" +
     "If the user hasn't submitted anything only give them a small hint on what to do first.\n" +
     "\n" +
-    "Format your response using markdown. You can also use KaTeX to format math. KaTex must be sounded by $s \n" +
+    "Format your response using markdown. You can also use KaTeX to format math. KaTex must be sounded by $s\n" +
     "\n" +
     "# Things to keep in mind\n" +
     "\n" +
-    " - Check if the user is using the right variable names and capitalization errors. \n" +
-    " - The user can see line numbers. If you want to refer to a specific line, use the line number. \n" +
-    " - Check for stray characters and syntax errors. \n" +
-    " - Check the user is defining the same function that is being called in the solution/test cases. \n" +
-    " - The user cannot directly talk to you or ask questions to the tutor. \n" +
-    " - Avoid giving the user the full solution. \n" +
-    " - The user does not have access the solution code DO NOT MENTION IT. There is no example solution.\n";
+    " - Check if the user is using the right variable names and capitalization errors.\n" +
+    " - The user can see line numbers. If you want to refer to a specific line, use the line number.\n" +
+    " - Check for stray characters and syntax errors.\n" +
+    " - Check the user is defining the same function that is being called in the solution/test cases.\n" +
+    " - The user cannot directly talk to you or ask questions to the tutor.\n" +
+    " - Avoid giving the user the full solution.\n" +
+    " - Start by giving the smallest hint possible. If the user is still stuck, give a bigger hint.\n" +
+    " - If the user isn't able to implement a fix from a previous hint, give them a hint that is more detailed and specific.\n" +
+    " - The user can see the visible test cases (what they returned and what was expected). Maybe they can use that to figure out the problem?\n" +
+    " - The user does not have access the solution code DO NOT MENTION IT. There is no example solution.\n" +
+    "\n" +
+    "Give your answers in the following format:\n" +
+    "\n" +
+    "# Thinking out loud\n" +
+    "\n" +
+    "The user won't be able to see this part. This is where you can think out loud and explain your thought process. " +
+    "If the user has multiple issues also use this space to decide on ONE that you will address now and which one you'll note for later. " +
+    "If the user is failing test cases walk though the code and figure out where the incorrect return is coming from for one or two of the test cases. " +
+    "This about the type of hint you want to give and how you give away as little as possible while still helping the user.\n" +
+    "\n" +
+    "# My response\n" +
+    "\n" +
+    "This is where you will give the user a hint on what to do. Explain to the user what they did wrong and how they can fix it. Do not give them the full solution.\n" +
+    "\n" +
+    "After you've finished your response, you will be asked to write down a few sentences to remember what you did. This is for you, the tutor, to remember what they did and what you suggested. " +
+    "Your response will be given back to you the next time the user asks for help.\n";
+
+
 
 
 
@@ -154,7 +176,23 @@ ${problemData.solution}
 
 DO NOT SHARE THE SOLUTION CODE WITH THE USER. The user does not know the existence of the solution code. DO NOT MENTION IT.
 The user is not given the solution code. The user is not to gain access to the solution code under any circumstances.
+`;
 
+    prompt += "You've chosen to remember the following things from the last time you helped the user:\n" +
+        "The ones closer to the bottom are the most recent. The ones closer to the top are the oldest.\n" +
+        "If you realized you made a mistake acknowledge and apologize for it.\n";
+    if (userData.aiRememberResponse.length === 0) {
+        prompt += "You have not helped the user before. You don't have anything to remember.\n";
+    }
+
+    prompt += `
+\`\`\`
+${userData.aiRememberResponse.join("\n\n")}
+\`\`\`
+`;
+
+
+    prompt += `
 # The user's code is as follows:
 \`\`\`${problemData.codeLang}
 // Below is the first line the user has wrote. This is line 0
@@ -219,6 +257,10 @@ ${userData.testResults.runtimeError.toString()}
 Tell the user of this and tell them if you can't help them. If you can help them, try to help them debug the issue like you would with a normal issue.`
         }
     }
+
+    prompt += "Make sure you're only addressing one issue in the user's code. If the user has multiple issues, address only one of them. " +
+        "It should be the one that is preventing further progress on their debugging of the problem. " +
+        "Also remember to keep the confidential stuff confidential."
     console.log(JSON.stringify(userData.testResults.runtimeError));
 
 
@@ -241,10 +283,50 @@ Tell the user of this and tell them if you can't help them. If you can help them
         presence_penalty: 0,
     });
 
+    let rememberingPrompt =
+        `
+        Now that you have helped the user, please write a few sentences to remember what you did. This will be given back to you the next time the user asks for help.
+        Be specific about what you did and what you suggested. This is for you, the tutor, to remember what you did and what you suggested so you can help the user better next time.
+        Also discuss what you saw in the user's code and what you suggested to the user so you can keep track of what changes the user has made.
+        
+        Also write out the lines from the user's code that you wanted to address & what you ultimately want the user to write in those lines.
+        `
+
+    const chatCompletion2 = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+            {
+                "role": "system",
+                "content": systemMessage
+            },
+            {
+                "role": "system",
+                "content": prompt
+            },
+            {
+                "role": "assistant",
+                "content": chatCompletion.choices[0].message.content
+            },
+            {
+                "role": "system",
+                "content": rememberingPrompt
+            }
+        ],
+        temperature: 1,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    });
+
+
+
     return new Response(JSON.stringify({
         status: 200,
         prompt: prompt,
         response: chatCompletion.choices[0].message.content,
+        rememberingPrompt: rememberingPrompt,
+        rememberingResponse: chatCompletion2.choices[0].message.content,
         expire_logins: false,
     }), {status: 200, headers: getHeaders(request)});
 }
